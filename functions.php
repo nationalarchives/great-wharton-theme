514c67e982944e884484dd85f729f341642af282
Chris Bishop <domingobishop@gmail.com>
2016-05-27 12:57:27 +0100

    copied function from tan-base

--- functions.php
+++ functions.php
@@ -75,4 +75,51 @@ function get_top_category_post($cat, $id, $tab){
 
 show_admin_bar(false);
 
+// Edit as required
+function tnatheme_globals() {
+    global $pre_path;
+    global $pre_crumbs;
+    if (substr($_SERVER['REMOTE_ADDR'], 0, 3) === '10.') {
+        $pre_path = '';
+        $pre_crumbs = array(
+            'Great Wharton' => '/'
+        );
+    } else {
+        $pre_crumbs = array(
+            'First World War' => '/first-world-war/',
+            'Great Wharton' => '/great-wharton/',
+        );
+        $pre_path = '/first-world-war/great-wharton';
+    }
+}
+if ( $_SERVER['SERVER_ADDR'] !== $_SERVER['REMOTE_ADDR'] ) {
+    tnatheme_globals(); } else {
+    $pre_path = '';
+    $pre_crumbs = array(
+        'Great Wharton' => '/'
+    );
+}
+
+// Make styles and scripts paths relative
+add_filter( 'script_loader_src', 'tna_styles_scripts_relative' );
+add_filter( 'style_loader_src', 'tna_styles_scripts_relative' );
+function tna_styles_scripts_relative( $url ) {
+    return str_replace( site_url(), '', $url );
+}
+// Make template URLs relative
+function make_path_relative( $url ) {
+    global $pre_path;
+    return str_replace( site_url(), $pre_path, $url );
+}
+// Fix URLs in wp_head
+function tna_wp_head() {
+    ob_start();
+    wp_head();
+    $wp_head = ob_get_contents();
+    ob_end_clean();
+    global $pre_path;
+    $wp_head = str_replace( site_url(), 'http://www.nationalarchives.gov.uk' . $pre_path, $wp_head );
+    echo $wp_head;
+}
+
 ?>
