3c87ca6d15d37d03d93c1ec8fa937d6c6589b66a
Chris Bishop <domingobishop@gmail.com>
2016-06-02 15:40:22 +0100

    house keeping

--- page.php
+++ page.php
@@ -3,139 +3,118 @@
 
 get_header(); ?>
 <div class="page-branding">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-6 tna-brand-page">
-                <a href="http://nationalarchives.gov.uk" title="Visit The National Archives website"><img src="<?php bloginfo("stylesheet_directory");?>/images/tna-logo-white.png" alt="The National Archives logo" class="img-responsive"></a>
-            </div>
-            <div class="col-xs-6 about-page">
-                <a href="<?php echo make_path_relative( esc_url(home_url('/', 'http')) );
-                // use category slug for hashtag
-                $category = get_the_category();
-                echo "#".$category[0]->slug;?>" title="Back to Great Wharton">
-                <img src="<?php bloginfo("stylesheet_directory");?>/images/tna-wharton-title-colour.png" alt=" Great Wharton title" title="View the introduction to Great Wharton">
-                </a>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-6 tna-brand-page">
+				<a href="http://nationalarchives.gov.uk" title="Visit The National Archives website"><img
+						src="<?php bloginfo( "stylesheet_directory" ); ?>/images/tna-logo-white.png"
+						alt="The National Archives logo" class="img-responsive"></a>
+			</div>
+			<div class="col-xs-6 about-page">
+				<a href="<?php echo make_path_relative( esc_url( home_url( '/', 'http' ) ) );
+				// use category slug for hashtag
+				$category = get_the_category();
+				echo "#" . $category[0]->slug; ?>" title="Back to Great Wharton">
+					<img src="<?php bloginfo( "stylesheet_directory" ); ?>/images/tna-wharton-title-colour.png"
+					     alt=" Great Wharton title" title="Back to Great Wharton">
+				</a>
+			</div>
+		</div>
+	</div>
 </div>
 <main role="main">
+	<?php if ( have_posts() ): while ( have_posts() ) : the_post();
+		$current_page = get_the_ID();
+		?>
+		<!-- main loop goes here -->
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12">
+					<div class="info">
+						<div class="inner">
+							<a href="<?php echo make_path_relative( esc_url( home_url( '/', 'http' ) ) );
+							// use category slug for hashtag
+							$category = get_the_category();
+							echo "#" . $category[0]->slug; ?>" title="Back to Great Wharton"
+							   role="button" class="button">&middot; Back to Great Wharton &middot;</a>
+							<?php
+							// show dropdown only if the page has a category i.e. story content
+							if ( has_category() ) { ?>
+								<div class="info-toolbar-right">
+									<div class="info_select">
+										<select onchange="if (this.value) window.location.href=this.value">
+											<option value="">Select a story</option>
+											<?php
+											$pageargs   = array(
+												'post_type'    => 'page',
+												'order'        => 'ASC',
+												'orderby'      => 'menu_order',
+												'post__not_in' => array( get_option( 'page_on_front' ) ),
+											);
+											$page_query = new WP_Query( $pageargs );
+											if ( $page_query->have_posts() ) {
+												while ( $page_query->have_posts() ) {
+													$page_query->the_post();
+													$page_id = $page_query->post->ID;
+													//show only pages with category i.e. hide all non-story content
+													if ( has_category() ) {
+														?>
+														<option
+															value="<?php echo make_path_relative( get_permalink() ); ?>"><?php the_title(); ?></option>
+														<?php
+													}
+												}
+											}
+											wp_reset_postdata();
+											?>
+										</select>
+									</div>
+								</div>
+								<hr>
+							<?php } ?>
+							<h1><?php the_title(); ?></h1>
+							<!-- featured image -->
+							<?php if ( has_post_thumbnail() ) {
+								$image_id      = get_post_thumbnail_id( $page->ID );
+								$image_src     = wp_get_attachment_image_src( $image_id, 'full', false );
+								$image_src     = fix_internal_url( $image_src[0] );
+								$image_caption = get_post( get_post_thumbnail_id() )->post_excerpt;
+								?>
+								<div class="feature-image">
+									<img src="<?php echo make_path_relative( $image_src ); ?>" width="100%"
+									     alt="Image of <?php echo( $image_caption ); ?>"
+									     title="<?php echo( $image_caption ); ?>">
+									<p class="caption alignright"><?php echo( $image_caption ); ?></p>
+								</div>
+								<?php
+							}
+							?>
+							<!-- featured image -->
 
-    <?php if (have_posts()): while (have_posts()) : the_post();
+							<div class="content-columns clearfix">
+								<?php the_content(); ?>
+								<hr>
+							</div>
 
-        $current_page = get_the_ID();
+							<a href="<?php echo make_path_relative( esc_url( home_url( '/', 'http' ) ) );
+							// use category slug for hashtag
+							$category = get_the_category();
+							echo "#" . $category[0]->slug; ?>" title="Back to Great Wharton"
+							   role="button" class="button">&middot; Back to Great Wharton &middot;</a>
 
-        //echo $current_page;
-        ?>
+							<span class='st_sharethis_large'>Share this</span>
 
-        <!-- main loop goes here -->
-        <div class="container">
-        <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <!-- school -->
-                    <div class="info">
-                        <!--                    <a href="-->
-                        <?php //echo esc_url( home_url( '/' ) ); ?><!--" title="Back to Great Wharton">-->
-                        <!--                        <div class="close"><i class="fa fa-times fa-2x"></i></div>-->
-                        <!--                    </a>-->
+						</div>
+					</div>
 
-                        <div class="inner">
-
-                            <a href="<?php echo make_path_relative( esc_url(home_url('/', 'http')) );
-                            // use category slug for hashtag
-                            $category = get_the_category();
-                            echo "#".$category[0]->slug;?>" title="Back to Great Wharton"
-                               role="button" class="button">&middot; Back to Great Wharton &middot;</a>
-
-                            <?php
-                            // show dropdown only if the page has a category i.e. story content
-                            if (has_category()) {?>
-                            <div class="info-toolbar-right">
-                                <div class="info_select">
-                                    <select onchange="if (this.value) window.location.href=this.value">
-                                        <option value="">Select a story</option>
-                                        <?php
-                                        $pageargs = array(
-                                            'post_type' => 'page',
-                                            'order' => 'ASC',
-                                            'orderby' => 'menu_order',
-                                            'post__not_in' => array(get_option('page_on_front')),
-
-                                        );
-                                        $page_query = new WP_Query($pageargs);
-                                        if ($page_query->have_posts()) {
-                                            while ($page_query->have_posts()) {
-                                                $page_query->the_post();
-                                                $page_id = $page_query->post->ID;
-                                                //show only pages with category i.e. hide all non-story content
-                                                if (has_category()) {
-                                                    ?>
-
-                                                    <option value="<?php echo make_path_relative( get_permalink() ); ?>"><?php the_title(); ?></option>
-
-                                                <?php
-                                                }
-                                            }
-                                        }
-
-                                        wp_reset_postdata();
-                                        ?>
-                                    </select>
-                                </div>
-                            </div>
-                                <hr>
-                            <?php }?>
-                            <h1><?php the_title(); ?></h1>
-
-                            <!-- featured image -->
-                            <?php if (has_post_thumbnail()) {
-
-                                $image_id = get_post_thumbnail_id($page->ID);
-                                $image_src = wp_get_attachment_image_src($image_id, 'full', false);
-                                $image_src = fix_internal_url($image_src[0]);
-                                $image_caption = get_post(get_post_thumbnail_id())->post_excerpt;
-
-                                ?>
-                                <img src="<?php echo make_path_relative( $image_src ); ?>" width="100%"  alt="Image of <?php echo($image_caption); ?>" title="<?php echo($image_caption); ?>">
-                                <p class="caption alignright"><?php echo($image_caption); ?></p>
-                                <?php
-                            }
-                            ?>
-
-
-                            <!-- featured image -->
-
-<div class="clear-both"></div>
-
-                            <div class="content-columns">
-
-                                <?php the_content(); ?>
-                                <hr>
-                            </div>
-
-                            <a href="<?php echo make_path_relative( esc_url(home_url('/', 'http')) );
-                            // use category slug for hashtag
-                            $category = get_the_category();
-                            echo "#".$category[0]->slug;?>" title="Back to Great Wharton"
-                               role="button" class="button">&middot; Back to Great Wharton &middot;</a>
-
-                            <span class='st_sharethis_large'>Share this</span>
-
-                        </div>
-                    </div>
-
-                    <!-- school -->
-                </div>
-            </div>
-        </div>
-
-
-    <?php endwhile; ?>
-
-    <?php else: ?>
-
-
-    <?php endif; ?>
+					<!-- school -->
+				</div>
+			</div>
+		</div>
+	<?php endwhile; ?>
+	<?php else:
+		// do nothing ?>
+	<?php endif; ?>
 </main>
 
 <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
@@ -147,17 +126,17 @@ get_header(); ?>
 <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
 <script src="<?php echo make_path_relative( get_template_directory_uri() ); ?>/js/scripts.js"></script>
 <script type="text/javascript">
-    stLight.options({
-        publisher: "e1514b1f-8114-4751-a7dc-7af051944bf6",
-        doNotHash: false,
-        doNotCopy: false,
-        hashAddressBar: false,
-        onhover: false
-    });
+	stLight.options({
+		publisher: "e1514b1f-8114-4751-a7dc-7af051944bf6",
+		doNotHash: false,
+		doNotCopy: false,
+		hashAddressBar: false,
+		onhover: false
+	});
 
 </script>
 <script>
-    $(".st_sharethis_large").show("slow");
+	$(".st_sharethis_large").show("slow");
 </script>
 
 <?php get_footer(); ?>
