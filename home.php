4ed27211811422f07c791a5770f200303746267e
Chris Bishop <domingobishop@gmail.com>
2016-06-30 15:31:34 +0100

    formatting

--- home.php
+++ home.php
@@ -1,9 +1,7 @@
 <?php
-
 /*
  Template Name: Home
  */
-
 get_header(); ?>
 <?php if (have_posts()): while (have_posts()) : the_post();?>
     <div class="overlay">
@@ -104,28 +102,16 @@ get_header(); ?>
             <!-- Instructions end -->
         </div>
     </div>
-
-
+	
     <main role="main">
-
-
-
-        <!-- Main wrapper start here -->
-
-
-
-            <h1 class="sr-only"><?php the_title();?></h1>
-
-
-
+    <!-- Main wrapper start here -->
+	    <h1 class="sr-only"><?php the_title();?></h1>
 
         <!-- Canvas viewer start here-->
         <div class="canvas-viewer" id="wrapper">
             <!--
             Pass Category, CSS ID and Tab
-
             CSS ID must NOT be same as hashtag, otherwise it will interfere with the repositioning.
-
             -->
             <?php get_top_category_post('postman', 'houses', '6');?>
             <?php get_top_category_post('schools', 'children', '7');?>
@@ -140,55 +126,44 @@ get_header(); ?>
             <?php get_top_category_post('field', 'child-labour', '16');?>
             <?php get_top_category_post('hunt', 'forest', '17');?>
 
-
-
             <div class="main_background"></div>
         </div>
         <!-- Canvas viewer end here -->
 
-
-
-        <!-- Main wrapper end here -->
+    <!-- Main wrapper end here -->
     </main>
 
-<!--    <div class="outer">-->
-        <?php $key="more_url";
-
-        if (get_post_meta($post->ID, $key, true)) {
-
-            ?>
-            <div class="learn_more_wrap">
-                <div class="learn_more">
-                    <?php echo get_post_meta($post->ID, "more_text", true);?>
-                    <a href="<?php   echo get_post_meta($post->ID, $key, true);?>" role="button" class="button" tabindex="18">&middot; <?php echo get_post_meta($post->ID, "more_button", true);?> &middot;</a>
-                </div>
-                <div class="learn_more_close">
-                    <i class="fa fa-times fa-2x"></i>
-                </div>
+	<!-- Learn more here -->
+    <?php $key="more_url";
+    if (get_post_meta($post->ID, $key, true)) { ?>
+        <div class="learn_more_wrap">
+            <div class="learn_more">
+                <?php echo get_post_meta($post->ID, "more_text", true);?>
+                <a href="<?php   echo get_post_meta($post->ID, $key, true);?>" role="button" class="button" tabindex="18">&middot; <?php echo get_post_meta($post->ID, "more_button", true);?> &middot;</a>
             </div>
-
-        <?php
-        }?>
-
-        <div class="map-zoom">Zoom <i class="fa fa-plus" aria-hidden="true" title="Zoom in"></i> <i class="fa fa-minus" aria-hidden="true" title="Zoom out"></i></div>
-
-        <div class="about">
-            <img src="<?php echo make_path_relative( get_template_directory_uri() ); ?>/images/tna-wharton-title-colour.png" alt=" Great Wharton title" title="View the introduction to Great Wharton" tabindex="5">
+            <div class="learn_more_close">
+                <i class="fa fa-times fa-2x"></i>
+            </div>
         </div>
+    <?php }?>
+	<!-- Learn more end here -->
 
-        <div class="tna_brand">
-            <a href="http://nationalarchives.gov.uk" title="Visit The National Archives website" tabindex="4">
-                <img src="<?php echo make_path_relative( get_template_directory_uri() ); ?>/images/tna-logo-white.png" alt="The National Archives logo">
-            </a>
-        </div>
-        <!-- Learn more end here-->
-<!--    </div>-->
+	<div class="map-zoom">Zoom <i class="fa fa-plus" aria-hidden="true" title="Zoom in"></i> <i class="fa fa-minus" aria-hidden="true" title="Zoom out"></i></div>
+
+	<!-- Branding here -->
+    <div class="about">
+        <img src="<?php echo make_path_relative( get_template_directory_uri() ); ?>/images/tna-wharton-title-colour.png" alt=" Great Wharton title" title="View the introduction to Great Wharton" tabindex="5">
+    </div>
+    <div class="tna_brand">
+        <a href="http://nationalarchives.gov.uk" title="Visit The National Archives website" tabindex="4">
+            <img src="<?php echo make_path_relative( get_template_directory_uri() ); ?>/images/tna-logo-white.png" alt="The National Archives logo">
+        </a>
+    </div>
+	<!-- Branding end here -->
 
 <?php endwhile; ?>
 
 <?php endif; ?>
-
-
 
 <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
 <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
@@ -201,12 +176,6 @@ get_header(); ?>
 <script src="<?php echo make_path_relative( get_template_directory_uri() ); ?>/js/tiles.js"></script>
 <script src="<?php echo make_path_relative( get_template_directory_uri() ); ?>/js/scripts.js"></script>
 <script src="<?php echo make_path_relative( get_template_directory_uri() ); ?>/js/great-wharton.js"></script>
-
-
-
-
-
-
 
 <?php get_footer(); ?>
 
