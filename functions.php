8e48276cf82ed1e4f09df1f3a3804314e8afec10
domingobishop <domingobishop@gmail.com>
2021-06-14 19:13:30 +0100

    nginx

--- functions.php
+++ functions.php
@@ -82,7 +82,14 @@ function tnatheme_globals()
 {
     global $pre_path;
     global $pre_crumbs;
-    if (substr($_SERVER['REMOTE_ADDR'], 0, 3) === '10.') {
+    $headers = apache_request_headers();
+    if ( isset($_SERVER['HTTP_X_NGINX_PROXY']) && isset($headers['X_HOST_TYPE']) && $headers['X_HOST_TYPE'] == 'public' ) {
+        $pre_crumbs = array(
+            'First World War' => '/first-world-war/',
+            'Home front stories' => '/home-front-stories/',
+        );
+        $pre_path = '/first-world-war/home-front-stories';
+    } elseif (substr($_SERVER['REMOTE_ADDR'], 0, 3) === '10.') {
         $pre_path = '';
         $pre_crumbs = array(
             'Home front stories' => '/'
